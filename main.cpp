8ef715b5805f4d6f3bc3aaa97d39c61a2b148d3a
MateuszKaszuba <118138321+MateuszKaszuba@users.noreply.github.com>
2022-12-14 21:56:10 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,3 @@
-/*Gra Arcnoid*/
-
-
-
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <iostream>
@@ -10,14 +6,18 @@ using namespace sf;
 class pilka {
 private:
 	sf::Vector2f position;
-	float xVel = 5;
-	float yVel = 5;
+	float xVel = 20;
+	float yVel = 20;
 	sf::CircleShape ball;
+	sf::RectangleShape HB;
+
 public:
 	pilka(float x_in, float y_in);
 	void przesun(float x_in, float y_in);
 	sf::CircleShape getPilka() { return ball; }
+	sf::RectangleShape gethitbox() { return HB; }
 	sf::Vector2f getPos() { return ball.getPosition(); }
+
 };
 class podloga {
 private:
@@ -28,21 +28,27 @@ public:
 	void przesun1(float x_in, float y_in);
 	sf::RectangleShape getPodloga() { return Odbijacz; }
 	sf::Vector2f getPos() { return Odbijacz.getPosition(); }
+
 };
 pilka::pilka(float x_in, float y_in)
-{
-	position.x = x_in;
+{	position.x = x_in;
 	position.y = y_in;
 	ball.setRadius(20);
 	ball.setFillColor(sf::Color(150, 50, 250));
 	ball.setOutlineThickness(2);
 	ball.setOutlineColor(sf::Color(250, 150, 100));
 	ball.setPosition(position);
+	HB.setSize(sf::Vector2f(40, 40));
+
+
+	HB.setPosition(position);
 }
+
 podloga::podloga(float x_in, float y_in)
 {
 	position.x = x_in;
 	position.y = y_in;
+	sf::FloatRect box1(Odbijacz.getGlobalBounds());
 	sf::Vector2f(100, 100);
 	Odbijacz.setSize(sf::Vector2f(100, 20));
 	Odbijacz.setFillColor(sf::Color::Black);
@@ -51,7 +57,6 @@ podloga::podloga(float x_in, float y_in)
 	Odbijacz.setOutlineColor(sf::Color(250, 150, 100));
 
 }
-
 void pilka::przesun(float x_in, float y_in)
 {
 	sf::Vector2f pos;
@@ -73,6 +78,7 @@ int main()
 	podloga d1(350, 500);
 	int dx = 10, dy = 10;
 	int dx1 = 5, dy1 = 0;
+	
 	sf::Clock zegar;
 
 
@@ -86,46 +92,48 @@ int main()
 				window.close();
 			}
 		}
-		if (zegar.getElapsedTime().asMilliseconds() > 20.0f) {
+		if (zegar.getElapsedTime().asMilliseconds() > 30.0f) {
 
-			if (p1.getPos().x > window.getSize().x - 50 || p1.getPos().x - 20 < 0)
+			if (zegar.getElapsedTime().asMilliseconds() > 30.0f)
+ {
 
-				dx = -dx;
-			if (p1.getPos().y > window.getSize().y - 50 || p1.getPos().y - 10 < 0)
-				dy = -dy;
-			p1.przesun(dx, dy);
-			zegar.restart();
-		}
-		sf::Clock zegar1;
-		if (zegar.getElapsedTime().asMilliseconds() > 20.0f) {
-			if (event.type == sf::Event::KeyPressed)
-			{
-				if (event.key.code == sf::Keyboard::A)
-				{
-					d1.przesun1(dx1, dy1);
-				}
-				if (event.key.code == sf::Keyboard::D)
-				{
-					d1.przesun1(dx1, dy1);
-				}
+			
+					if (p1.getPos().x > window.getSize().x -30 || p1.getPos().x  < 0)
+
+						dx = -dx;
+			
+					if (p1.getPos().y > window.getSize().y -40|| p1.getPos().y  < 0)
+						dy = -dy;
+				p1.przesun(dx, dy);
 				zegar.restart();
 			}
-		}
-			if (p1.getPos().y == d1.getPos().y+10 || d1.getPos().x + 30 >p1.getPos().x> d1.getPos().x-30)
+	
+				
+					if (sf::Keyboard::isKeyPressed (sf::Keyboard::Key::A))
+					{
+						d1.przesun1(-dx1, dy1);
+					}
+					if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+					{
+						d1.przesun1(dx1, dy1);
+					}
+					
+			
+				
+					if (box1.intersects(pilka))
 			{
 				dx = -dx;
 				dy = dy;
-				p1.przesun(dx, dy);
 			}
 		
 		
-		
-		
-		window.clear(sf::Color::Black);
-		window.draw(p1.getPilka());
-		window.draw(d1.getPodloga());
-		window.display();
+
+
+			window.clear(sf::Color::White);
+			window.clear(sf::Color::Black);
+			window.draw(p1.getPilka());
+			window.draw(d1.getPodloga());
+			window.display();
+		}
 	}
 }
-
-
