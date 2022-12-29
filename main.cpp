85214d20b7734b6b976527e4493ddccc7983139a
MateuszKaszuba <118138321+MateuszKaszuba@users.noreply.github.com>
2022-12-29 13:47:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -37,21 +37,20 @@ private:
 	sf::RectangleShape pudelko;
 public:
 	pudelka(float x_in, float y_in);
+	void przesun1(float x_in, float y_in);
 	sf::RectangleShape getPudelko() { return pudelko; }
 	sf::Vector2f getPos() { return pudelko.getPosition(); }
 
 };
 pilka::pilka(float x_in, float y_in)
-{	position.x = x_in;
-	position.y = y_in;
+{
+
 	ball.setRadius(20);
 	ball.setFillColor(sf::Color(150, 50, 250));
 	ball.setOutlineThickness(2);
 	ball.setOutlineColor(sf::Color(250, 150, 100));
 	ball.setPosition(position);
 	HB.setSize(sf::Vector2f(40, 40));
-
-
 	HB.setPosition(position);
 }
 pudelka::pudelka(float x_in, float y_in)
@@ -59,9 +58,10 @@ pudelka::pudelka(float x_in, float y_in)
 	position.x = x_in;
 	position.y = y_in;
 	sf::Vector2f(100, 100);
-	pudelko.setSize(sf::Vector2f(30,40));
+	pudelko.setSize(sf::Vector2f(30, 40));
 	pudelko.setFillColor(sf::Color::Yellow);
 	pudelko.setPosition(position);
+
 
 }
 podloga::podloga(float x_in, float y_in)
@@ -75,6 +75,7 @@ podloga::podloga(float x_in, float y_in)
 	Odbijacz.setPosition(position);
 	Odbijacz.setOutlineThickness(2);
 	Odbijacz.setOutlineColor(sf::Color(250, 150, 100));
+
 
 }
 void pilka::przesun(float x_in, float y_in)
@@ -91,12 +92,28 @@ void podloga::przesun1(float x_in, float y_in)
 	pos.y = y_in;
 	Odbijacz.move(pos);
 }
+void pudelka::przesun1(float x_in, float y_in)
+{
+	sf::Vector2f pos;
+	pos.x = x_in;
+	pos.y = y_in;
+	pudelko.move(pos);
+}
 int main()
 {
 	RenderWindow window(VideoMode{ 800, 600 }, "Gra");
-	pilka p1(400, 300);
+	pilka p1(200, 200);
 	podloga d1(350, 500);
-	pudelka pd1(700, 500);
+	pudelka pd1(700, 100);
+	pudelka pd2(600, 100);
+	pudelka pd3(500, 100);
+	pudelka pd4(400, 100);
+	pudelka pd5(300, 100);
+	pudelka pd6(200, 100);
+	pudelka pd7(100, 100);
+
+
+
 	int dx = 10, dy = 10;
 	int dx1 = 10, dy1 = 0;
 
@@ -125,15 +142,24 @@ int main()
 
 				if (p1.getPos().y > window.getSize().y - 40 || p1.getPos().y < 0)
 					dy = -dy;
-				
-				if ((d1.getPos().x + 70 > p1.getPos().x)&& (p1.getPos().x > d1.getPos().x - 70) && ( d1.getPos().y - 30<p1.getPos().y))
+
+				if ((d1.getPos().x + 70 > p1.getPos().x) && (p1.getPos().x > d1.getPos().x - 70) && (d1.getPos().y - 30 < p1.getPos().y))
 				{
 					dx = -dx;
 					dy = -dy;
-		
+
 				}
 				p1.przesun(dx, dy);
 				zegar.restart();
+			}
+			if (((pd7.getPos().x + 20 > p1.getPos().x) && (pd7.getPos().x > d1.getPos().x - 20)) && ((pd7.getPos().y - 20 < p1.getPos().y) && (pd7.getPos().y  < p1.getPos().y+20)))
+			{
+
+				dx = -dx;
+				dy = -dy;
+				p1.przesun(dx, dy);
+				pd7.przesun1(1000, 1000);
+
 			}
 
 
@@ -142,26 +168,32 @@ int main()
 				if (d1.getPos().x < 0)
 					d1.przesun1(0, 0);
 				else
-				
-				d1.przesun1(-dx1, dy1);
-			
+
+					d1.przesun1(-dx1, dy1);
+
 			}
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
 			{
 				if (d1.getPos().x > window.getSize().x - 30)
 					d1.przesun1(0, 0);
 				else
-				d1.przesun1(dx1, dy1);
-				
+					d1.przesun1(dx1, dy1);
+
 			}
 
-	
-				window.clear(sf::Color::White);
-				window.clear(sf::Color::Black);
-				window.draw(p1.getPilka());
-				window.draw(d1.getPodloga());
-				window.draw(pd1.getPudelko());
-				window.display();
-			}
+
+			window.clear(sf::Color::White);
+			window.clear(sf::Color::Black);
+			window.draw(p1.getPilka());
+			window.draw(d1.getPodloga());
+			window.draw(pd1.getPudelko());
+			window.draw(pd2.getPudelko());
+			window.draw(pd3.getPudelko());
+			window.draw(pd4.getPudelko());
+			window.draw(pd5.getPudelko());
+			window.draw(pd6.getPudelko());
+			window.draw(pd7.getPudelko());
+			window.display();
 		}
 	}
+}
